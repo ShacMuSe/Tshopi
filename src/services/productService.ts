e20c8657a4ca849a61f4e44bf2150054a80a8dcd
Mohamed ALi Samet <stage_data_1@kaisensdata.fr>
2026-01-05 21:51:12 +0100

    fixed URL

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import type { Product } from "../types/Product";
-const API_URL = "http://localhost:8081/api/products";
+const API_URL = "https://tshopi-back-production.up.railway.app/api/products";
 
 export const getProducts = () =>
     axios.get<Product[]>(API_URL);
