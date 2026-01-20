eaac567890c88dbda3dc76dabb391d576bd5622f
Mohamed ALi Samet <stage_data_1@kaisensdata.fr>
2026-01-20 16:24:53 +0100

    added category in homePage

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import type { Order } from "../types/Order";
 
-const API_URL = "http://localhost:8081/api/orders";
-//const API_URL = "https://tshopi-back-production.up.railway.app/api/orders";
+//const API_URL = "http://localhost:8081/api/orders";
+const API_URL = "https://tshopi-back-production.up.railway.app/api/orders";
 
 export const createOrder = (order: Order) => {
     return axios.post(API_URL, order);
